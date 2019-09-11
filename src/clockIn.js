d34c203924c28094af669f5277b09fcfc39e9a49
Peter Kofler <peter.kofler@code-cop.org>
2019-09-11 21:11:50 +0200

    Moar TODOs for next session ;-)

--- src/clockIn.js
+++ src/clockIn.js
@@ -4,8 +4,10 @@ function clockIn(ajax, success, failure) {
     var timestamp = "01.01.2019 12:55";
     var userId = 1123;
 
-    // TODO: extract payload to local variable
+    // TODO extract payload to local variable
     // TODO extract failure callback to dedicated/module function
+    // TODO setup VSCode to see warning about any type -> add annotations
+    // TODO we are in world of Promise, we are not using callbacks but returning the Promise!
 
     ajax(endpointUrl, {
         timestamp: timestamp,
---
+++ vscode-setup.md
@@ -0,0 +1,21 @@
+# VS Code Setup
+
+Maybe need to install for warnings
+
+* TSLint
+
+Nice plugins
+
+* Todo Tree
+* Code Spell Checker
+* makrdownlint
+
+Fun plugins
+
+* Bracket Pair Colorizer
+* vscode-icons
+
+Not tried
+
+* jshint
+* Mocha* - had problems
